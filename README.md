2a8f77c39603106e4cfa48996a80caa71350b7ae
Jakob K <jjk@outlook.at>
2024-03-10 21:41:50 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## LPC1114/333 Project Template
+using CMake + VSCode instead of LPCXpresso. Based on NXP's startup code & chip library. Debugging is intended over NXP's LinkServer or your probe directly if it has
+a gdbserver in-built.
---
+++ src/main.c
@@ -0,0 +1,34 @@
+/*
+===============================================================================
+ Name        : main.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#ifdef __USE_CMSIS
+#include "LPC11xx.h"
+#endif
+
+#if defined(NO_BOARD_LIB)
+#include "chip.h"
+#else
+#include "board.h"
+#endif
+
+#include <cr_section_macros.h>
+
+int main(void) {
+
+    SystemCoreClockUpdate();
+
+    // Force the counter to be placed into memory
+    volatile static int i = 0 ;
+    // Enter an infinite loop, just incrementing a counter
+    while(1) {
+        i++ ;
+    }
+    return 0 ;
+}
